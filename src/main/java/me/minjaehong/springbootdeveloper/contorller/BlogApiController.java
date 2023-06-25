8e4ebd509faa354d649ae8cee032d650160c54de
JaeHong94 <moonshin123@naver.com>
2023-06-25 23:46:19 +0900

    글 목록 조회api 및 테스트코드

--- src/main/java/me/minjaehong/springbootdeveloper/contorller/BlogApiController.java
+++ src/main/java/me/minjaehong/springbootdeveloper/contorller/BlogApiController.java
@@ -3,12 +3,16 @@ package me.minjaehong.springbootdeveloper.contorller;
 import lombok.RequiredArgsConstructor;
 import me.minjaehong.springbootdeveloper.domain.Article;
 import me.minjaehong.springbootdeveloper.dto.AddArticleRequest;
+import me.minjaehong.springbootdeveloper.dto.ArticleResponse;
 import me.minjaehong.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,5 +25,15 @@ public class BlogApiController {
     Article savedArticle = blogService.save(request);
     // 요청한 자원이 성공적으로 생성되었으며 저장된 블로그 글 정보를 응답 객체에 담아 전송
     return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
+  }
+
+  @GetMapping("/api/articles")
+  public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+    List<ArticleResponse> articles = blogService.findAll()
+            .stream()
+            .map(ArticleResponse::new)
+            .toList();
+
+    return ResponseEntity.ok().body(articles);
   }
 }

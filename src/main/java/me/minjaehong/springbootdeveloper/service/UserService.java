4b655ac2e944cb7328cc5083b406e3156208f562
JaeHong94 <moonshin123@naver.com>
2023-08-01 18:22:12 +0900

    Spring Security를 활용한 로그인/로그아웃/회원가입 구현

---
+++ src/main/java/me/minjaehong/springbootdeveloper/service/UserService.java
@@ -0,0 +1,22 @@
+package me.minjaehong.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.minjaehong.springbootdeveloper.domain.User;
+import me.minjaehong.springbootdeveloper.dto.AddUserRequest;
+import me.minjaehong.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+  private final UserRepository userRepository;
+  private final BCryptPasswordEncoder bCryptPasswordEncoder;
+  public Long save(AddUserRequest dto) {
+    return userRepository.save(User.builder()
+            .email(dto.getEmail())
+            .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+            .build()).getId();
+  }
+}
